f8856986ac48aba300a448fd740c550ddce6337c
Jhonatan Aguiar Lopes <jhonatan.jal@gmail.com>
2018-06-06 22:29:19 -0300

    implementando apresentação da nota

--- js/app/controllers/notas-controller.js
+++ js/app/controllers/notas-controller.js
@@ -18,22 +18,19 @@ class NotasController {
             peso: $('#peso-nota2').value,
             nota: $('#peso-nota2').value
         }
-        console.log(this._calculaMedia(nota1, nota2));
+        let result = this._calculaMedia(nota1, nota2);
+        this.resultadoView.update(result);
     }
 
     _calculaMedia(nota1, nota2) {
-        var resultado = 0;
-
-        if (nota2.nota.length == 0) {
-            resultado = this._CalculaNotaNecessaria(nota1.nota);
-        } else {
-            resultado = ((nota1.nota * nota1.peso) + (nota2.nota * nota2.peso)) / 10;
+        var resultado = {};
+        
+        let nota = ((nota1.nota * nota1.peso) + (nota2.nota * nota2.peso)) / 10;
+        resultado =  {
+            nota: nota.toPrecision(3),
+            mensagem: 'Sua Nota é'
         }
 
-        return resultado.toPrecision(3);
-    }
-
-    _CalculaNotaNecessaria(nota) {
-        return (6 - (nota * 0.4)) / 0.6;
+        return resultado;
     }
 }
