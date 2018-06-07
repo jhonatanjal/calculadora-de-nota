94ad572f67e5cbbe0c990177e8bc488ba8ad2f2e
Jhonatan Aguiar Lopes <jhonatan.jal@gmail.com>
2018-06-06 21:11:06 -0300

    corrigindo erros do querySelector

--- js/app/controllers/notas-controller.js
+++ js/app/controllers/notas-controller.js
@@ -1,24 +1,22 @@
 class NotasController {
     constructor() {
-        this.campoPesoNota1 = document.querySelector('#peso-nota1')
-        this.campoNota1 = document.querySelector('#nota1');
-
-        this.campoPesoNota2 = document.querySelector('#peso-nota2')
-        this.campoNota2 = document.querySelector('#nota2');
-
         let view = document.querySelector('.view')
         this.inputNotaView = new InputNotaView(view);
         this.resultadoView = new ResultadoView(view);
+
     }
 
-    calculaNota() {
+    calculaNota(event) {
+        event.preventDefault();
+        let $ = document.querySelector.bind(document);
+
         let nota1 = {
-            peso: this.campoPesoNota1.value,
-            nota: this.campoNota1.value
+            peso: $('#peso-nota1').value,
+            nota: $('#nota1').value
         }
         let nota2 = {
-            peso: this.campoPesoNota2.value,
-            nota: this.campoNota2.value
+            peso: $('#peso-nota2').value,
+            nota: $('#peso-nota2').value
         }
         console.log(this._calculaMedia(nota1, nota2));
     }
--- js/app/view/input-nota-view.js
+++ js/app/view/input-nota-view.js
@@ -6,20 +6,20 @@ class InputNotaView extends View {
         <form class="notas-form">
             <div class="campo-nota1 form-group">
                 <label for="peso1">Peso</label>
-                <input id="peso1" class="peso form-control" type="number" value="4" disabled>
+                <input id="peso-nota1" class="peso form-control" type="number" value="4" disabled>
 
                 <label for="nota1">Nota</label>
-                <input id="nota2" class="nota form-control" type="number">
+                <input id="nota1" class="nota form-control" type="number">
             </div>
             <div class="campo-nota2">
                 <label for="peso2">Peso</label>
-                <input id="peso2" class="peso form-control" type="number" value="6" disabled>
+                <input id="peso-nota2" class="peso form-control" type="number" value="6" disabled>
 
                 <label for="nota2">Nota</label>
                 <input id="nota2" class="nota form-control" type="number">
             </div>
             <div class="bnt-calcula-nota">
-                <button class="btn" onclick="notasController.calculaNota()">Calcular</button>
+                <button class="btn" onclick="notasController.calculaNota(event)">Calcular</button>
             </div>                
         </form>
         `;
