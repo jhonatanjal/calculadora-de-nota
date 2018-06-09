e998672f633f1ec2d4d39ba624ba57cb1306fd9c
Jhonatan Lopes <jhonatan.jal@gmail.com>
2018-06-09 19:23:51 -0300

    calcula nota necessária para média

--- js/app/controllers/notas-controller.js
+++ js/app/controllers/notas-controller.js
@@ -11,24 +11,43 @@ class NotasController {
         let $ = document.querySelector.bind(document);
 
         let nota1 = {
-            peso: $('#peso-nota1').value,
+            peso: $('#peso-nota1').value / 10,
             nota: $('#nota1').value
         }
+
         let nota2 = {
-            peso: $('#peso-nota2').value,
+            peso: $('#peso-nota2').value / 10,
             nota: $('#nota2').value
         }
+
         let result = this._calculaMedia(nota1, nota2);
         this.resultadoView.update(result);
     }
 
     _calculaMedia(nota1, nota2) {
         var resultado = {};
-        
-        let nota = ((nota1.nota * nota1.peso) + (nota2.nota * nota2.peso)) / 10;
-        resultado =  {
+
+        if (nota2.nota.length == 0) {
+            resultado = this._calculaNotaNecessaria(nota1);
+        } else {
+            let nota = ((nota1.nota * nota1.peso) + (nota2.nota * nota2.peso));
+    
+            resultado = {
+                nota: nota.toPrecision(3),
+                mensagem: 'Sua Nota é'
+            }
+        }
+
+        return resultado;
+    }
+
+    _calculaNotaNecessaria(nota1) {
+        let media = 6;
+        let nota = (media - (nota1.nota * nota1.peso)) / (1 - nota1.peso);
+
+        let resultado = {
             nota: nota.toPrecision(3),
-            mensagem: 'Sua Nota é'
+            mensagem: 'Nota necessária para ser aprovado com média ' + media
         }
 
         return resultado;
--- js/app/view/resultado-view.js
+++ js/app/view/resultado-view.js
@@ -4,12 +4,12 @@ class ResultadoView extends View {
 
         return `
         <div class="row justify-content-md-center">
-            <div class="col-sm-3">
+            <div class="col-sm-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title nota-mensagem">${model.mensagem}</h5>
                         <p class="card-text nota-result">${model.nota}</p>
-                        <button class="btn btn-primary" onclick="notasController.inputNotaView.update();">Voltar</button>
+                        <button class="btn btn-primary" onclick="notasController.inputNotaView.update()">Voltar</button>
                     </div>
                 </div>
             </div>
