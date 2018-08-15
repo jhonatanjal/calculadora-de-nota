4ca1a54d90f2ce6d33b4e29cf5f365a2fa331302
Jhonatan Lopes <jhonatan.jal@gmail.com>
2018-08-15 11:26:05 -0300

    melhorando a navegação com tab

--- js/app/view/input-nota-view.js
+++ js/app/view/input-nota-view.js
@@ -6,14 +6,14 @@ class InputNotaView extends View {
             <form class="notas-form" onsubmit="notasController.calculaNota(event)">
                 <div class="campo-nota">
                     <label for="peso1">Peso</label>
-                    <input id="peso-nota1" class="peso form-control" type="number" value="4" min="1" max="10">
+                    <input id="peso-nota1" class="peso form-control" type="number" value="4" min="1" max="10" tabindex="-1">
 
                     <label for="nota1">Nota</label>
                     <input id="nota1" class="nota form-control" type="number" step="0.01" min="0" max="10" required autofocus>
                 </div>
                 <div class="campo-nota">
                     <label for="peso2">Peso</label>
-                    <input id="peso-nota2" class="peso form-control" type="number" value="6" min=1" max="10">
+                    <input id="peso-nota2" class="peso form-control" type="number" value="6" min=1" max="10" tabindex="-1">
 
                     <label for="nota2">Nota</label>
                     <input id="nota2" class="nota form-control" type="number" step="0.01" min="0" max="10">
